f48f045c66e3737eebed42a2b231449daa9c2d0a
weiseng18 <tws.hci@gmail.com>
2020-09-13 20:23:02 +0800

    [feature prep] added drawArrayData method to draw bars to represent numerical array data

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@ function drawCanvas(id, height, width, padding) {
 	this.padding = padding;
 
 	this.rectWidth = padding;
-	this.rectMaxHeight = height - 2*padding;
+	this.rectMaxHeight = height - 3*padding;
 }
 
 drawCanvas.prototype.init = function() {
@@ -20,6 +20,35 @@ drawCanvas.prototype.init = function() {
 
 	document.body.appendChild(c);
 };
+
+// base of each rectangle should be aligned
+// height of each rectangle should be relative to the data value
+drawCanvas.prototype.drawArrayData = function(arr) {
+	this.ctx.fillStyle = "#0066cc";
+	this.ctx.textAlign = "center";
+	this.ctx.font = "15px Arial";
+
+	var max = Math.max(...arr);
+	for (var i=0; i<arr.length; i++) {
+		// scale height
+		var height = arr[i]/max * this.rectMaxHeight;
+		var width = this.rectWidth;
+
+		var bottomRightX = 2*this.padding * (i+1);
+		// -2*this.padding to allocate vertical height to draw the number
+		var bottomRightY = this.height - 2*this.padding;
+
+		var topLeftX = bottomRightX - width;
+		var topLeftY = bottomRightY - height;
+
+		this.ctx.fillRect(topLeftX, topLeftY, width, height);
+
+		// calculate position to draw the number
+		var numX = bottomRightX - width / 2;
+		var numY = bottomRightY + this.padding;
+		this.ctx.fillText(arr[i], numX, numY);
+	}
+}
 
 var canvas;
 
