533ee55a955481f91743886fb5031a2bc120382d
weiseng18 <tws.hci@gmail.com>
2020-09-13 19:12:31 +0800

    [feature prep] create canvas via javascript during window.onload

---
+++ js/main.js
@@ -0,0 +1,21 @@
+function drawCanvas(id, height, width) {
+	this.id = id;
+	this.height = height;
+	this.width = width;
+}
+
+drawCanvas.prototype.init = function() {
+	var c = document.createElement("canvas");
+	c.id = this.id;
+	c.height = this.height;
+	c.width = this.width;
+
+	document.body.appendChild(c);
+};
+
+var canvas;
+
+window.onload = function() {
+	canvas = new drawCanvas("draw", 400, 1000);
+	canvas.init();
+}
