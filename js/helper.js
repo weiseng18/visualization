c51962d99a1df362d2c61647bf43516bb20afe54
weiseng18 <tws.hci@gmail.com>
2020-09-16 17:18:23 +0800

    [feature prep] added random2D() function to generate a random 2D array with limits on the numbers

--- js/helper.js
+++ js/helper.js
@@ -1,3 +1,13 @@
 function get(id) {
 	return document.getElementById(id);
 }
+
+function random2D(length, min, max) {
+	var arr = [];
+	var range = max - min + 1;
+	for (var i=0; i<length; i++) {
+		var num = Math.floor(Math.random() * range) + min;
+		arr.push(num);
+	}
+	return arr;
+}
